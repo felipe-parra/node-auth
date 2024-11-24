b10afab2aa6c6616aa50e61801eace2f6e18dba2
Felipe Parra <fpr29.10@gmail.com>
2024-11-24 00:21:32 -0600

    ♻️ refactor(jwt): extract secret and add token validation
    - extract JWT secret to a constant for improved readability
    - add validateToken method to handle token verification gracefully

--- src/config/jwt.ts
+++ src/config/jwt.ts
@@ -1,22 +1,29 @@
 import jwt from 'jsonwebtoken'
 import { envs } from './envs'
 
+const SECRET = envs.JWT_SECRET
+
 export class JwtAdapter {
   static async generateToken(
     payload: Object,
     duration: string = '2h'
   ): Promise<string | null> {
     return new Promise((resolve) => {
-      jwt.sign(
-        payload,
-        envs.JWT_SECRET,
-        { expiresIn: duration },
-        (err, token) => {
-          if (err) return resolve(null)
+      jwt.sign(payload, SECRET, { expiresIn: duration }, (err, token) => {
+        if (err) return resolve(null)
 
-          resolve(token!)
-        }
-      )
+        resolve(token!)
+      })
+    })
+  }
+
+  static validateToken<T>(token: string): Promise<T | null> {
+    return new Promise((resolve) => {
+      jwt.verify(token, SECRET, (err, decoded) => {
+        if (err) return resolve(null)
+
+        resolve(decoded as T)
+      })
     })
   }
 }
