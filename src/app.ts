1d5c6f0284d66b8d41ca6669c4b9922fd4ec51d9
Felipe Parra <fpr29.10@gmail.com>
2024-11-23 21:23:47 -0600

    ✨ feat(database): integrate MongoDB connection
    - add MongoDB connection using MongoDatabase module
    - configure database name and URL from environment variables

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,17 @@
 import { envs } from './config'
+import { MongoDatabase } from './data/mongodb'
 import { AppRoutes } from './presentation/routes'
 import { Server } from './presentation/server'
-
 ;(() => {
   main()
 })()
 
 async function main() {
-  // todo: await base de datos
+  await MongoDatabase.connect({
+    dbName: envs.MONGO_DB_NAME,
+    mongoUrl: envs.MONGO_URL,
+  })
 
-  // todo: inicio server
   new Server({
     port: envs.PORT,
     routes: AppRoutes.routes,
