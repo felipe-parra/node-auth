d4eb6315817c58cc827bf616587264cf122a8a6e
Felipe Parra <fpr29.10@gmail.com>
2024-11-24 20:01:31 -0600

    ✨ feat(auth): add login functionality to auth data source
    - add login method to AuthDataSourceImpl class
    - validate user credentials using email and password
    - utilize custom error handling for invalid credentials

--- src/infrastructure/datasources/auth.datasource.impl.ts
+++ src/infrastructure/datasources/auth.datasource.impl.ts
@@ -3,6 +3,7 @@ import { UserModel } from '../../data/mongodb'
 import {
   AuthDatasource,
   CustomError,
+  LoginUserDto,
   RegisterUserDto,
   UserEntity,
 } from '../../domain'
@@ -10,11 +11,29 @@ import { UserMapper } from '../mappers/user.mapper'
 
 type HashFunction = (password: string) => string
 type CompareFunction = (password: string, hashed: string) => boolean
+
 export class AuthDataSourceImpl implements AuthDatasource {
   constructor(
     private readonly hashPassword: HashFunction = BcryptAdapter.hash,
     private readonly comparePassword: CompareFunction = BcryptAdapter.compare
   ) {}
+
+  async login(loginUserDto: LoginUserDto): Promise<UserEntity> {
+    const { email, password } = loginUserDto
+
+    // Check mail
+    const user = await UserModel.findOne({ email })
+
+    if (!user) throw CustomError.badRequest("Email or Password don't match")
+
+    const isMatchPassword = this.comparePassword(password, user.password)
+
+    if (!isMatchPassword) {
+      throw CustomError.badRequest("Email or Password don't match")
+    }
+    return UserMapper.userEntityFromObject(user)
+  }
+
   async register(registerUserDto: RegisterUserDto): Promise<UserEntity> {
     const { name, email, password } = registerUserDto
 
