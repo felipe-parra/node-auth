5d884574cf696116c3a11a5f67369a7cd0aca476
Felipe Parra <fpr29.10@gmail.com>
2024-11-24 20:01:37 -0600

    ✨ feat(auth): add login functionality to auth repository
    - import LoginUserDto to handle login data transfer
    - implement login method to authenticate users via datasource

--- src/infrastructure/repositories/auth.repository.impl.ts
+++ src/infrastructure/repositories/auth.repository.impl.ts
@@ -1,11 +1,18 @@
-import { AuthDatasource, AuthRepository, RegisterUserDto, UserEntity } from "../../domain";
+import {
+  AuthDatasource,
+  AuthRepository,
+  LoginUserDto,
+  RegisterUserDto,
+  UserEntity,
+} from '../../domain'
 
-export class AuthRepositoryImpl implements AuthRepository{
-  constructor(
-    private readonly datasource:AuthDatasource
-  ){}
+export class AuthRepositoryImpl implements AuthRepository {
+  constructor(private readonly datasource: AuthDatasource) {}
+
+  async login(loginUserDto: LoginUserDto): Promise<UserEntity> {
+    return this.datasource.login(loginUserDto)
+  }
   async register(registerUserDto: RegisterUserDto): Promise<UserEntity> {
     return this.datasource.register(registerUserDto)
   }
-
 }
