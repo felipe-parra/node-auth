7d90dd45f37f9bf2cd7d55b5b90f9c1b5c92b069
Felipe Parra <fpr29.10@gmail.com>
2024-11-24 20:29:39 -0600

    ♻️ refactor(auth): streamline imports in auth.repository
    - consolidate imports into a single line for improved readability and organization

--- src/domain/repositories/auth.repository.ts
+++ src/domain/repositories/auth.repository.ts
@@ -1,6 +1,4 @@
-import { LoginUserDto } from '../dtos/auth/login-user.dto'
-import { RegisterUserDto } from '../dtos/auth/register-user.dto'
-import { UserEntity } from '../entities/user.entity'
+import { UserEntity, RegisterUserDto, LoginUserDto } from '../'
 
 export abstract class AuthRepository {
   abstract login(loginUserDto: LoginUserDto): Promise<UserEntity>
