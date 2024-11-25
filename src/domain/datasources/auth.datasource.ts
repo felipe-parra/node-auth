9cb8f3d6d92f1355fdaf3dbe84c1509e2cf1f1d1
Felipe Parra <fpr29.10@gmail.com>
2024-11-24 20:29:29 -0600

    💄 style(datasource): simplify import statements
    - consolidate imports from the same directory into a single line

--- src/domain/datasources/auth.datasource.ts
+++ src/domain/datasources/auth.datasource.ts
@@ -1,7 +1,4 @@
-import { LoginUserDto } from '../dtos/auth/login-user.dto'
-import { RegisterUserDto } from '../dtos/auth/register-user.dto'
-import { UserEntity } from '../entities/user.entity'
-
+import { UserEntity, RegisterUserDto, LoginUserDto } from '../'
 export abstract class AuthDatasource {
   abstract login(loginUserDto: LoginUserDto): Promise<UserEntity>
 
